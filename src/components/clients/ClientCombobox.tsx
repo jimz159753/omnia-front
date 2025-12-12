951e0d4c91ff269a7d2a812e3acb704692284362
Luis Jimenez <luisjc140992@gmail.com>
2025-12-12 10:34:01 -0600

    fix: Adjust popover width and refine select trigger styles
    - Updated the width of the PopoverContent in ClientCombobox for better layout consistency.
    - Refined the class names in SelectTrigger to enhance styling and maintain visual integrity across components.

--- src/components/clients/ClientCombobox.tsx
+++ src/components/clients/ClientCombobox.tsx
@@ -58,7 +58,7 @@ export const ClientCombobox: React.FC<ClientComboboxProps> = ({
             <FiCode className="ml-2 h-4 w-4 shrink-0 opacity-50 rotate-90" />
           </Button>
         </PopoverTrigger>
-        <PopoverContent className="w-full p-0" align="start">
+        <PopoverContent className="w-[24vw] p-0" align="start">
           <Command>
             <CommandInput placeholder="Search client..." className="h-9" />
             <CommandList>
